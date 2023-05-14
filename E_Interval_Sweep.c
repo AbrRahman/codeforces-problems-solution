ad4fb8a735585ca2e2ebd9bf39099f357618d8c6
AbrRahman <abrahman.est@gamil.com>
2023-05-14 21:54:05 +0600

    even odd possible

---
+++ E_Interval_Sweep.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int v;
+    if(a!=0||b!=0){
+        v = abs(a-b);
+    }
+    // }else{
+    //     v=2;
+    // }
+    if(v>=0&&v<=1){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+    return 0;
+}
