a6f523157814b5c754c74a52a9688d71aab5a0c2
AbrRahman <abrahman.est@gamil.com>
2023-05-16 23:00:29 +0600

    reversing num whith some condition

---
+++ B_Reversing.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int j=0,k;
+     for(int i=0;i<n;i++){
+          
+        if(arr[i]==0){
+            k=i-1;
+            while (j<k)
+            {
+               int temp =arr[j];
+                 arr[j]=arr[k];
+                  arr[k]=temp;
+                  j++;
+                  k--;
+            }
+            j=0;
+        }
+        
+    }
+     for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
